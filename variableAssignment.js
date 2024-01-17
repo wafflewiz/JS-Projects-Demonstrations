5027a24f735e79a2a62ea5472ca00ca6855352d1
Rami A <ramialj94@yahoo.com>
2024-01-17 11:31:49 -0600

    variable demo

---
+++ variableAssignment.js
@@ -0,0 +1,10 @@
+//Name: Rami Aljanaby
+//Date: 1/17/24
+
+var x= 1
+var x = 1 + 5;
+
+let y = prompt("What did you eat for breakfast?", "Enter your answer");
+
+console.log("The variable x has the value of " + x);
+console.log("The variable y has a value of " + y);
