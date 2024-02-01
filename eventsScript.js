979273586dc8cc1568ef35668e9ccffd8e1e779f
Rami A <ramialj94@yahoo.com>
2024-01-31 23:06:50 -0600

    corrected formatting

--- eventsScript.js
+++ eventsScript.js
@@ -8,6 +8,7 @@ var body = document.querySelector(".body");
 var para = document.createElement("p");
 var node = document.createTextNode("\nThis is a new paragraph.\n");
 var element = document.getElementById("container");
+
 //Loop that creates a new paragraph 5 times within the class "container"
 for (let i = 0; i < 5; i++){
     element.appendChild(para);
@@ -19,8 +20,8 @@ function changeColour(){
     if(document.body.style.backgroundColor == "yellow"&&
        element.style.backgroundColor == "aqua"){
 
-    document.body.style.backgroundColor = "white";
-    element.style.backgroundColor = "white";
+        document.body.style.backgroundColor = "white";
+        element.style.backgroundColor = "white";
     }
     else{
         element.style.backgroundColor = "aqua"
