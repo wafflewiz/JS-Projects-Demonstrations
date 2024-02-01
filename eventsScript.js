63175d46c6adbb1b2111e0d38f634dcce1ff9347
Rami A <ramialj94@yahoo.com>
2024-01-31 23:04:44 -0600

    fixed naming errors

--- eventsScript.js
+++ eventsScript.js
@@ -1,31 +1,23 @@
-/*
-Name: Rami Aljanaby
-Date: 1/31/24
-Description: This site demonstrates features of the DOM 
-*/
-
-
 var body = document.querySelector(".body");
-var para = document.createcontainer("p");
+var para = document.createElement("p");
 var node = document.createTextNode("\nThis is a new paragraph.\n");
-var container = document.getcontainerById("container");
-
-//For loop to create a paragraph element and append it to "container"
+var element = document.getElementById("container");
+//Loop that creates a new paragraph 5 times within the class "container"
 for (let i = 0; i < 5; i++){
-    container.appendChild(para);
+    element.appendChild(para);
     para.appendChild(node.cloneNode());
 }
 
-//This function changes the background color based on current colors
+//Changes colors of the page based on current colors
 function changeColour(){
     if(document.body.style.backgroundColor == "yellow"&&
-       container.style.backgroundColor == "aqua"){
+       element.style.backgroundColor == "aqua"){
 
-        document.body.style.backgroundColor = "white";
-        container.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "white";
+    element.style.backgroundColor = "white";
     }
     else{
-        container.style.backgroundColor = "aqua"
+        element.style.backgroundColor = "aqua"
         document.body.style.backgroundColor = "yellow"
     }
 }
