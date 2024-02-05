fbcd819ce103e8294d3f3d80d329d9fd271deeab
Rami A <ramialj94@yahoo.com>
2024-02-05 11:13:57 -0600

    added color randomizer script

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ Name: Rami Aljanaby
 Date: 2/5/24
 Description: This project demonstrates usage of JS in combination with HTML to manipulate the DOM.
 */
+const colors =[];
+
+const randomColor = () => {
+    const randomizer = "#" + Math.floor(Math.random()*19777215).toString(16);
+    colors.push(randomizer);
+}
+
+console.log(colors);
