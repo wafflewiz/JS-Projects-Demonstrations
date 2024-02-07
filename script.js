a10369c728e60aa12e3755ed32451710e4f71e73
Rami A <ramialj94@yahoo.com>
2024-02-06 20:10:13 -0600

    improved comments - added TODO

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 /*
 Name: Rami Aljanaby
 Date: 2/5/24
-Description: This project demonstrates usage of JS in combination with HTML to manipulate the DOM.
+Description: This web app changes the background by cycling through an array of colors.
+             The user may control the speed at which the colors cycle via a slider control. 
 */
+
+//TODO: use a node for document.body - consider changing the color of a small area instead of background
+
 // Initialize array, document objects, and variables to manipulate the DOM
 const colors =["red", "blue", "green", "white", "purple", "orange", "yellow", "pink", "brown","Chartreuse"];
+
+//Variable for the button to begin changing the background colors
 const button = document.querySelector("button");
+
+//Variable to store the slider which will be used to change color strobe speed
 var slider = document.getElementById("mySlider");
 colorIndex = 0;
 
-
+//Warns the user 
 var dialogbox = window.alert("CAUTION! This web app contains rapidly flashing colors!");
 
+//This function changes the background color by iterating through the colors array
 function changeColor() {
     // Sets background color to the color at current index
     document.body.style.backgroundColor = colors[colorIndex];
@@ -30,7 +39,7 @@ function changeColor() {
 }
 
 
-// Add event listener to button
+// Add event listener to button that runs the changeColor function on click
 button.addEventListener("click", changeColor);
  
 
