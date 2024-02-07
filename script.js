0a869d1bb5378b21ab7b527080c4f59629e7deb0
Rami A <ramialj94@yahoo.com>
2024-02-06 18:37:11 -0600

    added button and slider

--- script.js
+++ script.js
@@ -3,11 +3,45 @@ Name: Rami Aljanaby
 Date: 2/5/24
 Description: This project demonstrates usage of JS in combination with HTML to manipulate the DOM.
 */
-const colors =[];
+// Initialize array, document objects, and variables to manipulate the DOM
+const colors =["red", "blue", "green", "white", "purple", "orange", "yellow", "pink", "brown","Chartreuse"];
+const button = document.querySelector("button");
+var slider = document.getElementById("mySlider");
+colorIndex = 0;
+output.innerHTML = slider.value; // Display the default slider value
 
-const randomColor = () => {
-    const randomizer = "#" + Math.floor(Math.random()*19777215).toString(16);
-    colors.push(randomizer);
+
+var dialogbox = window.alert("CAUTION! This web app contains rapidly flashing colors!");
+
+function changeColor() {
+    // Sets background color to the color at current index
+    document.body.style.backgroundColor = colors[colorIndex];
+    
+    // Increments colorIndex
+    colorIndex++;
+    
+    // Reset colorIndex to 0 if it exceeds the length of colors array
+    if (colorIndex === colors.length) 
+    {
+        colorIndex = 0;
+    }
+    
+    // Call setTimeout recursively to change color after a delay based on slider input
+    setTimeout(changeColor, slider.value); 
 }
 
-console.log(colors);
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+slider.innerHTML = this.value;
+  }
+
+// Add event listener to button
+button.addEventListener("click", changeColor);
+ 
+
+
+
+
+
+
+
