dae3f93ee9236bfb0d9cf7066c10fd6ac7047113
Rami A <ramialj94@yahoo.com>
2024-02-11 18:23:36 -0600

    added comments and formatting

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ Date: 2/5/24
 Description: This web app changes the background by cycling through an array of colors.
              The user may control the speed at which the colors cycle via a slider control. 
 */
-addEventListener("DOMContentLoaded", (event) => {});
 
-
-//TODO: use a node for document.body - consider changing the color of a small area instead of background
-
-// Initialize array, document objects, and variables to manipulate the DOM
+// Initialize array of colors
 const colors =["Red", "Blue", "Green", "White", "Purple", "Orange", "Yellow", "Pink", "Brown","Chartreuse"];
 
 //Variable for the button to begin changing the background colors
@@ -22,6 +18,7 @@ var slider = document.getElementById("colorslider");
 const silderInfo = document.getElementById("sliderInfo");
 const colorInfo = document.getElementById("colorInfo");
 
+//Variable to store current index of array
 colorIndex = 0;
 
 //This function expression changes the background color by iterating through the colors array
@@ -30,6 +27,7 @@ const changeColor = function () {
         const currentColorTxt = document.createTextNode("Current color is: " + colors[colorIndex]);
         const currentSliderTxt = document.createTextNode("Current interval is: " + slider.value + " seconds");
 
+        //Adds text relating to current color and slider setting
         while (colorIndex <= colors.length ){
             sliderInfo.textContent="";
             sliderInfo.appendChild(currentSliderTxt);
@@ -39,6 +37,7 @@ const changeColor = function () {
 
             break;
         }
+
         // Sets background color to the color at current index
         document.body.style.backgroundColor = colors[colorIndex];
         
@@ -55,11 +54,10 @@ const changeColor = function () {
         setTimeout(changeColor, slider.value*1000); 
 
         // Display the current color as text
-        
-
         stopButton.addEventListener("click", () => {
             location.reload();
         }); 
+        
 }
 
 // Add event listener to button that runs the changeColor function on click
