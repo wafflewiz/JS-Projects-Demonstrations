2836216e0a1114c58971f535e41b940387e34551
Rami A <ramialj94@yahoo.com>
2024-02-14 11:17:59 -0600

    first commit

---
+++ gameScript.js
@@ -0,0 +1,13 @@
+mainPara = document.querySelector("#mainPara").innerHTML;
+
+
+
+var playerChar = {
+    hp: 100,
+    dmg: 25,
+    onhit: false,
+    bonusxp:false,
+    stagger:10,
+}
+
+console.log(mainPara);
