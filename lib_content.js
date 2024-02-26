a53aa7dcb06acf49fe2a03d4ad23d740751d95b6
Rami A <ramialj94@yahoo.com>
2024-02-25 20:39:23 -0600

    final version

--- lib_content.js
+++ lib_content.js
@@ -1,10 +1,12 @@
+/*Name : Rami Aljanaby
+    Description: Console outputs object properties created from class library  lib_content.js
+*/
 class Human {
     constructor(name) {  // Constructor
-      this.name = givenName;
+      this.name = name;
     }
     nameLog(){
-        console.log(givenName);
+        console.log(this.name);
     }
   }
  
-export default Human;
--- objects.js
+++ objects.js
@@ -1,2 +1,9 @@
-import Human from './lib_content.js';
+/*Name : Rami Aljanaby
+    Description: Console outputs object properties created from class library  lib_content.js
+*/
+const emp1 = new Human("John");
+const emp2 = new Human("Eric");
 
+
+emp1.nameLog();
+emp2.nameLog();
