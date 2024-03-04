3bf4d74805c82dcf2c6e5ff01e92ca8ddb002862
Rami A <ramialj94@yahoo.com>
2024-03-03 21:20:37 -0600

    final version

--- closures.js
+++ closures.js
@@ -1,21 +1,48 @@
-(Name, HP, ...moves)=>{
+//initialize function closure
+function createChar(Name, HP, ...moves){
+    //variables are private because local scope
     let hp = HP;
     let name = Name;
-    let moves = moves;
+    let movesArray = moves;
 
-    const total ={
+    //what the function will output
+    return{
         returnHp: ()=>{
-        this.hp
+            return hp;
         },
 
         returnName: ()=>{
-        this.name
+            return name;
         },
 
+        //displays elements of the moves array on the page
         appendMove: ()=>{
-        document.querySelector(".moves")
-        
-        }
+            //for each move in the moves array...
+            for (const move of movesArray){
+                //create a p element node
+                const node = document.createElement("p");
 
+                //create a text node of the move text
+                const textnode = document.createTextNode(move);
+
+                //find the moves class in the html
+                const movesEl = document.querySelector(".moves");
+
+                // add the text node to the p node 
+                node.appendChild(textnode);
+
+                //add the p node to the div node
+                movesEl.appendChild(node);
+            }
+        }
     }
 }
+
+//new character, assign args 
+let Bob = createChar("Bob", 100, "Jab", "Kick", "Overhead", "Low Jab", "Low Kick", "Block");
+
+//run the appendMove method of the object Bob to display moves on page
+Bob.appendMove();
+
+console.log("Name:", Bob.returnName());
+console.log("HP:", Bob.returnHp());
