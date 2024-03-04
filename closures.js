7f5f889f611c92cf672306d3bae8753a6d216024
Rami A <ramialj94@yahoo.com>
2024-03-03 19:22:08 -0600

    closures first commit

---
+++ closures.js
@@ -0,0 +1,21 @@
+(Name, HP, ...moves)=>{
+    let hp = HP;
+    let name = Name;
+    let moves = moves;
+
+    const total ={
+        returnHp: ()=>{
+        this.hp
+        },
+
+        returnName: ()=>{
+        this.name
+        },
+
+        appendMove: ()=>{
+        document.querySelector(".moves")
+        
+        }
+
+    }
+}
