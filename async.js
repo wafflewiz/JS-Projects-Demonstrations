3170af55595609b126ff59e782eb49fa1d157c14
Rami A <ramialj94@yahoo.com>
2024-03-17 05:37:10 -0500

    final version

--- async.js
+++ async.js
@@ -1,20 +1,35 @@
 /*
-Name: Rami
+Name: Rami Aljanaby
 Proj: Async functions
+Date: 3/17/24
 */
+async function main () {
+    var runner1 = false;
+    var runner2 = false;
+    var loser = "";
 
-var runner1 = false;
-var runner2 = false;
-var loser = "";
+    const runner1Go = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            loser="Runner 1 lost";
+            resolve(runner1 = true);}, 2000);
+            
+    });
 
-const runner1Go = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("runner1");}, 2000); 
-});
+    const runner2Go = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            loser="Runner 2 lost";
+            resolve(runner2 = true);}, 1500); 
+            
+    });
+  
+    return [
+        result1 = await runner1Go,
+        result2 = await runner2Go,
+        loser,
+    ];
+}
+var returnValue = [main()];
 
-const runner2Go = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("runner2");
-    }, 1500); 
-});
-
+main().then((returnValue)=>{
+    console.log(returnValue[2]);
+}) 
