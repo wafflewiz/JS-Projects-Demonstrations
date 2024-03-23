5eb9cdee99de98c07dd05e7d16b9dd3db0663f3c
Rami A <ramialj94@yahoo.com>
2024-03-23 00:12:27 -0500

    added animation

--- fighterMain.js
+++ fighterMain.js
@@ -2,43 +2,84 @@
 
 const player1 = new Fighter("P1", 100, "Jab", "Low Kick");
 const player2 = new Fighter("P2", 100, "Jab", "Low Kick");
+//size of img
+const scale = 1.4;
+//dimensions used for img and canvas
+const width = 132;
+const height = 132;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
+const cycleLoop = [0, 1, 0, 2];
+
+//index for the animation loop array
+let currentMoveIndex = 0;
+let currentLoopIndex = 0;
+var blocking = false;
+let frameCount = 0;
 let p1JabImg = new Image();
 p1JabImg.src = "assets/P1Sprite/Attack_1.png";
-console.log(p1JabImg);
-var blocking = false;
 p1JabImg.onload = function () {
   init();
 };
 
+//create a 2D canvas element 
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
-function init() {
-  context.drawImage(p1JabImg, 0, 0, 32, 58, 0, 0, 32, 58);
+//draws the 2D image onto canvas
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+    context.drawImage(p1JabImg,
+                  frameX * width, frameY * height, width, height,
+                  canvasX, canvasY, scaledWidth, scaledHeight);
 }
-p1KeyInput = window.addEventListener("keydown", function (event) {
-  if (event.key === "a") {
-    console.log("a was pressed");
-    p1UpdateMove("Jab");
-  } else if (event.key === "s") {
-    p1UpdateMove("Low Kick");
-    console.log("s was pressed");
-  } else if (event.key === " ") {
-    // Perform player attack
-  }
-});
 
-p2KeyInput = window.addEventListener("keydown", function (event) {
-  if (event.key === "j") {
-    console.log("j was pressed");
-    p2UpdateMove("Jab");
-  } else if (event.key === "k") {
-    p2UpdateMove("Low Kick");
-    console.log("j was pressed");
-  } else if (event.key === " ") {
-    // Perform player attack
-  }
-});
+// main function
+function init() {
+    drawFrame(cycleLoop[0], 0, 0, 0);
+
+    p1KeyInput = window.addEventListener("keydown", function (event) {
+        if (event.key === "a") {
+            window.requestAnimationFrame(animateJab);
+            p1UpdateMove("Jab");
+        } else if (event.key === "s") {
+          p1UpdateMove("Low Kick");
+          console.log("s was pressed");
+        } else if (event.key === " ") {
+          // Perform player attack
+        }
+      });
+      
+      p2KeyInput = window.addEventListener("keydown", function (event) {
+        if (event.key === "j") {
+          console.log("j was pressed");
+          p2UpdateMove("Jab");
+        } else if (event.key === "k") {
+          p2UpdateMove("Low Kick");
+          console.log("j was pressed");
+        } else if (event.key === " ") {
+          // Perform player attack
+        }
+      });
+}
+//
+function animateJab() {
+    frameCount++;
+    if (frameCount < 7) {
+      window.requestAnimationFrame(animateJab);
+      return;
+    }
+    frameCount = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+    currentLoopIndex++;
+    if (currentLoopIndex >= (cycleLoop.length)+1) {
+    //currentLoopIndex = 0;    
+   // context.clearRect(0, 0, canvas.width, canvas.height);
+   return;
+    }
+    window.requestAnimationFrame(animateJab);
+}
+
 
 function p1UpdateMove(useMove) {
   switch (useMove) {
