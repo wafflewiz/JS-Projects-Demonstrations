5f577ae90ee716439d55d7e6964a96ea72ba4be9
Rami A <ramialj94@yahoo.com>
2024-03-23 02:10:04 -0500

    added an object of arrays containing move frames

--- fighterMain.js
+++ fighterMain.js
@@ -3,23 +3,32 @@
 const player1 = new Fighter("P1", 100, "Jab", "Low Kick");
 const player2 = new Fighter("P2", 100, "Jab", "Low Kick");
 //size of img
-const scale = 1.4;
+const scale = 1.2;
 //dimensions used for img and canvas
 const width = 132;
 const height = 132;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
-const cycleLoop = [0, 1, 0, 2];
+cycleLoop= [0, 1, 0, 2]
+
+const animLoops= {
+  alP1Idle: [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6],  
+  alP1Jab: [0, 1, 0, 2],
+  alP1Kick: [0, 1, 0, 2, 0, 3, 0, 4],
+  alP1Hurt: [],
+  alP1Dead: [],
+}
 
 //index for the animation loop array
-let currentMoveIndex = 0;
 let currentLoopIndex = 0;
+nextMoveRdy = true;
 var blocking = false;
 let frameCount = 0;
 let p1JabImg = new Image();
 p1JabImg.src = "assets/P1Sprite/Attack_1.png";
 p1JabImg.onload = function () {
   init();
+  defaultState();
 };
 
 //create a 2D canvas element 
@@ -32,14 +41,16 @@ function drawFrame(frameX, frameY, canvasX, canvasY) {
                   frameX * width, frameY * height, width, height,
                   canvasX, canvasY, scaledWidth, scaledHeight);
 }
-
+function defaultState(){
+    currentLoopIndex=0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawFrame(cycleLoop[1], 0, 0, 0);
+    }
 // main function
 function init() {
-    drawFrame(cycleLoop[0], 0, 0, 0);
 
     p1KeyInput = window.addEventListener("keydown", function (event) {
         if (event.key === "a") {
-            window.requestAnimationFrame(animateJab);
             p1UpdateMove("Jab");
         } else if (event.key === "s") {
           p1UpdateMove("Low Kick");
@@ -63,21 +74,23 @@ function init() {
 }
 //
 function animateJab() {
-    frameCount++;
-    if (frameCount < 7) {
-      window.requestAnimationFrame(animateJab);
-      return;
+    while (nextMoveRdy){    
+        frameCount++;
+        if (frameCount < 7) {
+        window.requestAnimationFrame(animateJab);
+        return;
+        }
+        frameCount = 0;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawFrame(animLoops.alP1Jab[currentLoopIndex], 0, 0, 0);
+        currentLoopIndex++;
+        if (currentLoopIndex >= cycleLoop.length+1) {
+            nextMoveRdy=false;
+            defaultState();
+        }
+        window.requestAnimationFrame(animateJab);
+        break;
     }
-    frameCount = 0;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
-    currentLoopIndex++;
-    if (currentLoopIndex >= (cycleLoop.length)+1) {
-    //currentLoopIndex = 0;    
-   // context.clearRect(0, 0, canvas.width, canvas.height);
-   return;
-    }
-    window.requestAnimationFrame(animateJab);
 }
 
 
@@ -90,8 +103,10 @@ function p1UpdateMove(useMove) {
 
       break;
     case "Jab":
+      nextMoveRdy=true;
       player1.currentMove = "Jab";
       p1Attack();
+      window.requestAnimationFrame(animateJab);
       player1.currentMove = "";
 
       break;
